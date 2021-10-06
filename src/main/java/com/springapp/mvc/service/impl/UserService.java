b0a13ad8b7083c88a4d9ea8cfd8af83ae8f43bf0
hoang92-gtvt <hoang92.gtvt@gmail.com>
2021-10-06 15:04:28 +0700

    change version dependency spring framewwork and jackson

--- src/main/java/com/springapp/mvc/service/impl/UserService.java
+++ src/main/java/com/springapp/mvc/service/impl/UserService.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Service("userService")
 public class UserService implements IUserService {
     @Autowired
-    private UserMapper mapper;
+    private IUserMapper mapper;
 
     @Override
     public List<UsersDemo> getAllUsers() {
